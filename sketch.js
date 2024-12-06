fb755d57a3d8aae6fc729a247cad896b6274e19e
11324135 <11324135@nhu.edu.tw>
2024-12-06 15:55:45 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://11324135.github.io/frog11324135/model.json';
 
 
 function preload() {
